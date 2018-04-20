df80211792ff653c2cdb2be9afd73f2b41315b2e
Aashita Kesarwani <kesar01@gmail.com>
2018-04-20 18:06:12 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nytcomments',
+      version='0.1',
+      description='Package to retrieve comments from the NYT articles that also serves as API Wrapper for NYT article search and performs the function of the now deprecated NYT Community API',
+      url='https://github.com/AashitaK/nyt-comments',
+      author='Aashita Kesarwani',
+      author_email='kesar01@gmail.com',
+      license='MIT',
+      packages=['nytcomments'],
+      zip_safe=False)
