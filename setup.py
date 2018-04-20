df7cc2cb469dc1f018be0828a84a185bc1c1d6c9
Aashita Kesarwani <kesar01@gmail.com>
2018-04-20 18:46:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(name='nytcomments',
       version='0.1',
-      description='Package to retrieve comments from the NYT articles that also serves as API Wrapper for NYT article search and performs the function of the now deprecated NYT Community API',
+      description='Package to retrieve comments from the New York Time articles that also serves as API Wrapper for NYT article search and performs the function of the now deprecated NYT Community API',
       url='https://github.com/AashitaK/nyt-comments',
+      keywords='Scraper for comments from the New York Time articles, API Wrapper for NYT API',
       author='Aashita Kesarwani',
       author_email='kesar01@gmail.com',
       license='MIT',
