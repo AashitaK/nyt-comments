4b1d58c51c614e22c39cbe4c2fb468eff44d8173
Aashita Kesarwani <kesar01@gmail.com>
2018-04-20 18:52:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as file:
+        return file.read()
+
 setup(name='nytcomments',
       version='0.1',
       description='Package to retrieve comments from the New York Time articles that also serves as API Wrapper for NYT article search and performs the function of the now deprecated NYT Community API',
+      long_description=readme(),
       url='https://github.com/AashitaK/nyt-comments',
       keywords='Scraper for comments from the New York Time articles, API Wrapper for NYT API',
       author='Aashita Kesarwani',
